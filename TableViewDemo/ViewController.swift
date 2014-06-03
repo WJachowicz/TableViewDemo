73c782c8842e810f065cb2622bc8f325f3d4f79c
Wojciech Jachowicz <wojciech.jachowicz@gmail.com>
2014-06-03 20:35:56 +0200

    Initial Commit

---
+++ TableViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewDemo
+//
+//  Created by Wojciech Jachowicz on 03.06.2014.
+//  Copyright (c) 2014 Wojciech Jachowicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
